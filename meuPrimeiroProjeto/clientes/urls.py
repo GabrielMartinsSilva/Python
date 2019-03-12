310b559cec919deaf05edb2b03791103d9005313
Gabriel Martins <gamartins77@gmail.com>
2019-03-12 02:22:51 -0300

    adicionado Delete ao projeto

--- meuPrimeiroProjeto/clientes/urls.py
+++ meuPrimeiroProjeto/clientes/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from .views import lista_de_pessoas
 from .views import persons_new
 from .views import persons_update
+from .views import persons_delete
 
 
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path('list/', lista_de_pessoas, name="lista_de_pessoas"),
     path('new/', persons_new, name="person_new"),
     path('update/<int:id>/', persons_update, name="persons_update"),
+    path('delete/<int:id>/', persons_delete, name="persons_delete"),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
