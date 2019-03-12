bbcdcc793a29724054af70bcc541a86ab813b71b
Gabriel Martins <gamartins77@gmail.com>
2019-03-12 17:11:43 -0300

    Login para controle de acesso implementado

--- ProjetoFinal/gestao_clientes/clientes/views.py
+++ ProjetoFinal/gestao_clientes/clientes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Person
 from .forms import PersonForm
 
 
+@login_required
 def lista_de_pessoas(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons': persons})
 
 
-
+@login_required
 def persons_new(request):
     form = PersonForm(request.POST or None, request.FILES or None)
 
@@ -18,7 +20,7 @@ def persons_new(request):
 
     return render(request, 'form_pessoas.html', {'form': form})
 
-
+@login_required
 def persons_update(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonForm(request.POST or None, request.FILES or None, instance=person)
@@ -28,7 +30,7 @@ def persons_update(request, id):
         return redirect('lista_de_pessoas')
 
     return render(request, 'form_pessoas.html', {'form': form})
-
+@login_required
 def persons_delete(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonForm(request.POST or None, request.FILES or None, instance=person)
